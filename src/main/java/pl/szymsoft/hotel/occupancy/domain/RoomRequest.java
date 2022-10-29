84db5bdfc98e77755d693cb7205e302a7ab1aaab
Grzegorz Szymański <grszyman@gmail.com>
2022-10-29 11:49:16 +0200

    [green] RoomRequest implementation

--- src/main/java/pl/szymsoft/hotel/occupancy/domain/RoomRequest.java
+++ src/main/java/pl/szymsoft/hotel/occupancy/domain/RoomRequest.java
@@ -5,4 +5,9 @@ import javax.money.MonetaryAmount;
 // in real life, we would need more information about a request (at least an id)
 public record RoomRequest(MonetaryAmount maxPrice) {
 
+    public RoomRequest {
+        if (maxPrice.isNegativeOrZero()) {
+            throw new IllegalArgumentException("maxPrice has to be positive");
+        }
+    }
 }
