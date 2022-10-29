e6b647999050916f064f573f5f53454e5eb74f80
Grzegorz Szymański <grszyman@gmail.com>
2022-10-29 14:29:49 +0200

    [green] OccupancyPlan implementation

--- src/main/java/pl/szymsoft/hotel/occupancy/domain/OccupancyPlan.java
+++ src/main/java/pl/szymsoft/hotel/occupancy/domain/OccupancyPlan.java
@@ -1,17 +1,25 @@
 package pl.szymsoft.hotel.occupancy.domain;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.vavr.collection.List;
+import io.vavr.collection.Stream;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.javamoney.moneta.Money;
 
 import javax.money.MonetaryAmount;
 
-import static java.util.Collections.emptyList;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.util.Comparator.comparing;
 import static lombok.AccessLevel.PACKAGE;
+import static pl.szymsoft.utils.Ints.requirePositiveOrZero;
+import static pl.szymsoft.utils.Objects.require;
 
 @Value
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@SuppressFBWarnings("EI_EXPOSE_REP")
 public class OccupancyPlan {
 
     List<RoomRequest> fulfilledPremiumRequests;
@@ -20,13 +28,20 @@ public class OccupancyPlan {
     MonetaryAmount totalEconomyPrice;
 
     private OccupancyPlan(
-            Iterable<RoomRequest> fulfilledPremiumRequests,
-            Iterable<RoomRequest> fulfilledEconomyRequests
+            Stream<RoomRequest> fulfilledPremiumRequests,
+            Stream<RoomRequest> fulfilledEconomyRequests
     ) {
-        this.fulfilledPremiumRequests = List.ofAll(fulfilledPremiumRequests);
-        this.fulfilledEconomyRequests = List.ofAll(fulfilledEconomyRequests);
-        this.totalPremiumPrice = null;
-        this.totalEconomyPrice = null;
+        this.fulfilledPremiumRequests = fulfilledPremiumRequests.toList();
+        this.fulfilledEconomyRequests = fulfilledEconomyRequests.toList();
+        this.totalPremiumPrice = totalPriceOf(fulfilledPremiumRequests);
+        this.totalEconomyPrice = totalPriceOf(fulfilledEconomyRequests);
+    }
+
+    private static MonetaryAmount totalPriceOf(Stream<RoomRequest> roomRequests) {
+        return roomRequests
+                .map(RoomRequest::maxPrice)
+                .reduceOption(MonetaryAmount::add)
+                .getOrElse(() -> Money.of(0, "EUR"));
     }
 
     @Builder(access = PACKAGE)
@@ -36,6 +51,31 @@ public class OccupancyPlan {
             int freePremiumRooms,
             int freeEconomyRooms
     ) {
-        return new OccupancyPlan(emptyList(), emptyList());
+        require(premiumPrice, MonetaryAmount::isPositiveOrZero, "premiumPrice has to be positive or zero");
+        requirePositiveOrZero(freePremiumRooms, "freePremiumRooms");
+        requirePositiveOrZero(freeEconomyRooms, "freeEconomyRooms");
+
+        return Stream.ofAll(requests)
+                .sorted(comparing(RoomRequest::maxPrice).reversed())
+                .partition(request -> request.maxPrice().isGreaterThanOrEqualTo(premiumPrice))
+                .apply((premiumRequests, economyRequests)
+                        -> createBookingPlan(premiumRequests, economyRequests, freePremiumRooms, freeEconomyRooms));
+    }
+
+    private static OccupancyPlan createBookingPlan(
+            Stream<RoomRequest> premiumRequests,
+            Stream<RoomRequest> economyRequests,
+            int freePremiumRooms,
+            int freeEconomyRooms
+    ) {
+        final var extraPremiumRooms = freePremiumRooms - min(freePremiumRooms, premiumRequests.size());
+        final var extraEconomyRequests = max(freeEconomyRooms, economyRequests.size()) - freeEconomyRooms;
+        final var requestsToUpgrade = min(extraPremiumRooms, extraEconomyRequests);
+
+        return new OccupancyPlan(
+                premiumRequests.take(freePremiumRooms)
+                        .appendAll(economyRequests.take(requestsToUpgrade)),
+                economyRequests.drop(requestsToUpgrade)
+                        .take(freeEconomyRooms));
     }
 }
