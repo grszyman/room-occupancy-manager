8786cc7db113e9a133dfe4b13100fec81718709a
Grzegorz Szymański <grszyman@gmail.com>
2022-10-30 20:27:51 +0100

    [green] RoomRequestsProvider returning values from configuration

--- src/main/java/pl/szymsoft/hotel/occupancy/RoomOccupancyManagerConfiguration.java
+++ src/main/java/pl/szymsoft/hotel/occupancy/RoomOccupancyManagerConfiguration.java
@@ -1,17 +1,30 @@
 package pl.szymsoft.hotel.occupancy;
 
+import org.javamoney.moneta.Money;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.szymsoft.hotel.occupancy.domain.api.RoomRequest;
 import pl.szymsoft.hotel.occupancy.domain.ports.RoomRequestsProvider;
 
-import java.util.Collections;
+import java.math.BigDecimal;
+
+import static java.util.Arrays.stream;
 
 @Configuration
 class RoomOccupancyManagerConfiguration {
 
     @Bean
-    RoomRequestsProvider roomRequestsProvider() {
-        return Collections::emptyList;
-    }
+    RoomRequestsProvider roomRequestsProvider(
+            @Value("${occupancy-planner.room-requests}") String roomRequestsConfig) {
 
+        final var roomRequests = stream(roomRequestsConfig.split(","))
+                .map(String::trim)
+                .map(BigDecimal::new)
+                .map(value -> Money.of(value, "EUR"))
+                .map(RoomRequest::new)
+                .toList();
+
+        return () -> roomRequests;
+    }
 }
